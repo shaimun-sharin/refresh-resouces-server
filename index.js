e3ad6a5f1bcc844e7f9e946968a26a3f7bdb9c04
shaimun-sharin <shaimunsharin@gmail.com>
2022-05-02 00:32:38 +0600

    created a single item api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -30,6 +30,13 @@ async function run() {
       const cursor = fruitCollection.find(query);
       const foods = await cursor.toArray();
       res.send(foods);
+    });
+
+    app.get("/food/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const food = await fruitCollection.findOne(query);
+      res.send(food);
     });
   } finally {
   }
